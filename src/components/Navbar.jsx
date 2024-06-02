6f2c6c531d501a1f16425adc18a06441e362adcb
Suman-199 <suman.mmt199@gmail.com>
2024-06-02 23:15:59 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,53 @@ import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  useState();
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
   useEffect(() => {}, []);
   return (
     <>
-      <Link />
+      <nav
+        className={`${styles.paddingX}  w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      >
+        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive();
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+              Suman &nbsp;
+              <span className="sm:block hidden"> | Front End DEveloper</span>
+            </p>
+          </Link>
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((nav) => (
+              <li
+                key={nav.id}
+                className={`${
+                  active === nav.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <img
+              src={toggle ? close : menu}
+              alt="menu"
+              className="w-[28px] h-[28px] object-contain"
+            />
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
